5b0c171397a56cd316f1cbf51fcecf4a77b614f7
veenamathew1234 <55123236+veenamathew1234@users.noreply.github.com>
2023-09-27 11:51:08 +0530

    Update IntegrationE2EFlow.java to check if build error is captured

--- src/test/java/com/prime/tests/FPJ/IntegrationE2EFlow.java
+++ src/test/java/com/prime/tests/FPJ/IntegrationE2EFlow.java
@@ -62,7 +62,7 @@ public class IntegrationE2EFlow extends BaseTest {
         System.out.println("Web COUNT=" + totalResultsFromWebPage);
         searchServiceHelper = new SearchServiceHelper();
         response = searchServiceHelper.fetchSearchResults(platform, application, status);
-        BaseTest.assertEquals(WebDriverManager.getDriver(), response.getStatusCode(), 200, "Verifying search results API");
+        BaseTest.assertEquals(WebDriverManager.getDriver(), response.getStatusCode(), 400, "Verifying search results API");
         JsonPath js = new JsonPath(response.asString());
         totalResultsFromAPI = Integer.parseInt(js.get("pagination.totalResults").toString());
         BaseTest.assertEquals(WebDriverManager.getDriver(), totalResultsFromAPI, totalResultsFromWebPage, "Total Search results from api and webpage");
