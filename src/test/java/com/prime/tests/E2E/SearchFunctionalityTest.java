fd37de8cbe10de62cc82ab0fabd8b34587c590f8
veenamathew1234 <55123236+veenamathew1234@users.noreply.github.com>
2023-11-01 13:36:00 +0530

    Update SearchFunctionalityTest.java waiting for article

--- src/test/java/com/prime/tests/E2E/SearchFunctionalityTest.java
+++ src/test/java/com/prime/tests/E2E/SearchFunctionalityTest.java
@@ -223,7 +223,7 @@ public class SearchFunctionalityTest extends BaseTest {
         browseOrSearchPage.selectRefineTermValueFromRefineTermDDOnBrowseOrSearchResultPage(testData.get("testidvalueselect").toString(), testData.get("refinefilteroptionaffiliation").toString());
         browseOrSearchPage.enterRefineTermValueInRefineTermBoxOnBrowseOrSearchPage(testData.get("testidvalueenter").toString(), testData.get("refinefiltervalueaffiliation").toString());
         browseOrSearchPage.clickOnSearchButtonInRefineTermDDOnBrowseOrSearchPage();
-        WebDriverManager.getDriver().navigate().refresh();
+        Thread.sleep(5000);
         browseOrSearchPage.clickOnFirstArticleOnSearchOrBrowsePage();
         articleCitationPage.clickFirstAuthorOnArticlePage();
         String authorlabel = articleCitationPage.getauthorAffiliationPopupLabel();
