78320895405b35517603cf7b690d6e8a3cf86911
veenamathew1234 <55123236+veenamathew1234@users.noreply.github.com>
2023-12-04 18:37:53 +0530

    Update TetsingJava.java commenting unnecessary files

--- src/test/java/com/prime/tests/E2E/TetsingJava.java
+++ src/test/java/com/prime/tests/E2E/TetsingJava.java
@@ -1,55 +1,55 @@
-package com.prime.tests.E2E;
+// package com.prime.tests.E2E;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
+// import java.io.File;
+// import java.io.IOException;
+// import java.util.List;
 
-import javax.swing.text.html.Option;
+// import javax.swing.text.html.Option;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+// import org.apache.commons.io.FileUtils;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeOptions;
 
-import com.prime.generics.Helper;
-import com.vladsch.flexmark.util.sequence.BasedOptionsHolder.Options;
+// import com.prime.generics.Helper;
+// import com.vladsch.flexmark.util.sequence.BasedOptionsHolder.Options;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.qameta.allure.Allure;
+// import io.github.bonigarcia.wdm.WebDriverManager;
+// import io.qameta.allure.Allure;
 
-public class TetsingJava {
-//	private static int sizeCount;
-//
-//	public static void main(String[] args) {
-//		showContent(51, 50);
-//	}
-//
-//	public static void showContent(int actualCount, int filtersizecount) {
-//		int count = 0;
-//		if (actualCount > filtersizecount) {
-//			int size = actualCount / filtersizecount;
-//			for (int i = 0; i < size; i++) {
-//				sizeCount = actualCount - filtersizecount;
-//				actualCount = sizeCount;
-//				count++;
-//				if (actualCount < filtersizecount && actualCount>0) {
-//					count++;
-//				}
-//			}
-//			System.out.println("count :" + count);
-//		} else if (actualCount == filtersizecount) {
-//			count++;
-//			System.out.println("count :" + count);
-//		}
-//	}
+// public class TetsingJava {
+// //	private static int sizeCount;
+// //
+// //	public static void main(String[] args) {
+// //		showContent(51, 50);
+// //	}
+// //
+// //	public static void showContent(int actualCount, int filtersizecount) {
+// //		int count = 0;
+// //		if (actualCount > filtersizecount) {
+// //			int size = actualCount / filtersizecount;
+// //			for (int i = 0; i < size; i++) {
+// //				sizeCount = actualCount - filtersizecount;
+// //				actualCount = sizeCount;
+// //				count++;
+// //				if (actualCount < filtersizecount && actualCount>0) {
+// //					count++;
+// //				}
+// //			}
+// //			System.out.println("count :" + count);
+// //		} else if (actualCount == filtersizecount) {
+// //			count++;
+// //			System.out.println("count :" + count);
+// //		}
+// //	}
 
-public static void main(String[] args) {
+// public static void main(String[] args) {
 
-     ChromeOptions options = new ChromeOptions();
-     options.setHeadless(true);
-     ChromeDriver driver = new ChromeDriver();
-     driver.get("https://meridian:meridian2023@meridian-anesthesiaprogress-draft.prime-uat.pubfactory.com/");
+//      ChromeOptions options = new ChromeOptions();
+//      options.setHeadless(true);
+//      ChromeDriver driver = new ChromeDriver();
+//      driver.get("https://meridian:meridian2023@meridian-anesthesiaprogress-draft.prime-uat.pubfactory.com/");
  
-			}
+// 			}
 
-}
+// }
