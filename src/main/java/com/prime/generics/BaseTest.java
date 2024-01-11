9f79b970744823d4c10a0df775fd34e6062ac566
veenamathew1234 <55123236+veenamathew1234@users.noreply.github.com>
2024-01-11 14:14:12 +0530

    Update BaseTest.java enabling user agent

--- src/main/java/com/prime/generics/BaseTest.java
+++ src/main/java/com/prime/generics/BaseTest.java
@@ -417,7 +417,7 @@ public class BaseTest {
                     options.addArguments("--disable-extensions");
                     options.addArguments("--dns-prefetch-disable");
                     options.addArguments("--disable-gpu");
-                    //                   options.addArguments("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
+                                     options.addArguments("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
 
                     if (BaseTest.properties.getProperty("headLess").equalsIgnoreCase("Y")) {
                         options.addArguments("--headless");
