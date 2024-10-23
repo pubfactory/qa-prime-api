3a853e1c8c2973413c747d3a4de3dcf4391ff080
Veena Mathew <Veena.Mathew@kwglobal.com>
2024-10-23 11:33:59 +0530

    pushing userflows

--- src/test/java/com/prime/tests/userflows/PublisherUserFlow.java
+++ src/test/java/com/prime/tests/userflows/PublisherUserFlow.java
@@ -1,18 +1,7 @@
-/**
- * Testing the below functionality in Cite An Entry 
-
-1. Verifying if the citation button is present in the article page
-2. Verifying if citation pop-up is displayed when clicking on the citation button
-3. Verifying if user is able to see RIS,BIB,ENW Button is present under Export citation section.
-4. Verifying if user is able to download RIS,BIB,ENW formats
-5. Verifying if user is able to close citation pop-up after use.
-
- */
 package com.prime.tests.userflows;
 
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
-
+import com.prime.generics.BaseTest;
 import com.prime.generics.UserFlowDef;
 import com.prime.generics.WebDriverManager;
 import com.prime.retryAnalyzers.Retry;
@@ -21,24 +10,44 @@ import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
 
 public class PublisherUserFlow extends UserFlowDef {
-    private String testCaseId;
 
+	private String testCaseId;
 
-    @Severity(SeverityLevel.BLOCKER)
-    @Test(groups = {"fpj"}, enabled = true, retryAnalyzer = Retry.class,
-            description = "11  - Verify Publisher User flows")
-    @Story("EPIC-971")
-    public void publisherUserFlow() throws Exception {
-    	SoftAssert soft = new SoftAssert();
-        testCaseId = "1721296";
-        WebDriverManager.setTestcaseIdTestRail(testCaseId);
-        testDataInIt(testCaseId);
-        browserInit();
-        verifyCitationFunctionalityAndSelectEachFormatOfCitationAndCopyToClipBoardAndMakeSureItMatchesTheCitationPreview();
-        testDataInIt("1722758");
-        verifyPDFButonAvailableAndDownloadPDF();
-        testDataInIt("1722741");
-      //  VerifyVariousEmailButtonFeatures();
-        assertClose();
-    }
+	@Severity(SeverityLevel.BLOCKER)
+	@Test(groups = {
+			"AP" }, enabled = true, retryAnalyzer = Retry.class, description = "1733756  - Verify Publisher User flows")
+	@Story("EPIC-3818")
+	public void publisherUserFlow() throws Exception {
+		// SoftAssert soft = new SoftAssert();
+		// testCaseId = "1733756";
+		testCaseId = retrieveTCID(new Exception().getStackTrace()[0].getMethodName().split("-")[0].trim());
+		WebDriverManager.setTestcaseIdTestRail(testCaseId);
+		testDataInIt(testCaseId);
+		System.out.println("testcaseId =" + testCaseId);
+		browserInit();
+		String url = BaseTest.properties.getProperty(application);
+		String journalCurrentPage = testData.get("journalcurrentpage").toString();
+		driver.get(url + journalCurrentPage);
+		selectIssueDDAndVerifyCurrentIssueOnMostRecentVolumeList();
+		verifyEachListingHasTitleContributorDOIAndAbstractButton();
+		verifyAccessIconIsLockedWhenIamNotLoggedIn();
+		verifyTheFirstArticleLoadsCorrectlyAndClickedLinkMatchesTitleOfLoadedArticle();
+		verifyAllArticleAreFromTheIssueWhileNavigatingTheNextPrevControl();
+		System.out.println("testcaseId at the end =" + testCaseId);
+		String openAccessContent = testData.get("openaccesscontent").toString();
+		driver.get(url + openAccessContent);
+		verifyFulltextIsNavigableThroughArticleContent();
+		verifyAllFiguresAreLoadingInTheFigureTab();
+		verifyFigureOpenInPowePointOnceClicksOnItAndAbleToDownloadTheFigureInPPTFormatIfClicksOnDownloadButton();
+		clickContributorsAndPerformCurrentSiteSearch();
+		verifyCitationFunctionalityAndSelectEachFormatOfCitationAndCopyToClipBoardAndMakeSureItMatchesTheCitationPreview();
+		verifyShareButtonIsPresentAndEachOptionPromptsTheUSerToLogInInToTheRespectiveService();
+		verifyPDFButonAvailableAndDownloadPDF();
+		verifyGooglescholarAndPubmedSectionFunctionality();
+		verifyThatTheAutoLaunchNewEmailFunctionality();
+		verifyContentMetaDataServiceInReferences();
+		verifyThatDOIShouldBeLinkAndHandlingDOIRedirectFunctionality();
+		verifyThatTheCitationLinkFunctionalityIsWorkingFine();
+		assertClose();
+	}
 }
