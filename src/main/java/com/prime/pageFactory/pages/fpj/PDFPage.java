81b564cc2293ba632ad9effe3223ff8ab119569d
RakeshShevale03 <143789842+RakeshShevale03@users.noreply.github.com>
2025-02-21 15:33:34 +0530

    Update PDFPage.java updated the water mark method

--- src/main/java/com/prime/pageFactory/pages/fpj/PDFPage.java
+++ src/main/java/com/prime/pageFactory/pages/fpj/PDFPage.java
@@ -266,7 +266,9 @@ public class PDFPage extends BasePage {
      */
    public boolean verifyWatermarkIsPresentOnPreviewInPDFTabOnArticlePage(String waterMarkAppsName) throws Exception {
 	waitForDocumentReady();
-	   JavascriptExecutor js = (JavascriptExecutor) driver;
+	   JavascriptExecutor js = (JavascriptExecutor) driver;   
+            js.executeScript("window.scrollBy(0, 1000);");
+	     Thread.sleep(2000);
     	List<WebElement> element = driver.findElements(By.xpath("(//span[@role='presentation' and contains(text(),'" + waterMarkAppsName + "')])"));
 	System.out.println("Water element size :"+element.size());
 //        mouseOver(element.get(0),"Mouse hovering on the water mark");
