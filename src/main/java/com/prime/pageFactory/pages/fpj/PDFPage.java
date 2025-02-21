815c8c0ba7e1e00926df145545b4995fb78900d6
RakeshShevale03 <143789842+RakeshShevale03@users.noreply.github.com>
2025-02-21 16:31:36 +0530

    Update PDFPage.java added action library

--- src/main/java/com/prime/pageFactory/pages/fpj/PDFPage.java
+++ src/main/java/com/prime/pageFactory/pages/fpj/PDFPage.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import com.prime.generics.BasePage;
 import com.prime.generics.Helper;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.Keys;
 
 public class PDFPage extends BasePage {
 
