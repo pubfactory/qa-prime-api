0e3921bac61ee48cb80d964846c3080001571d6c
RakeshShevale03 <143789842+RakeshShevale03@users.noreply.github.com>
2025-02-25 18:27:37 +0530

    Update DiscoveryWidgetTest.java added doctor search keyword code

--- src/test/java/com/prime/tests/E2E/DiscoveryWidgetTest.java
+++ src/test/java/com/prime/tests/E2E/DiscoveryWidgetTest.java
@@ -167,8 +167,10 @@ public class DiscoveryWidgetTest extends BaseTest {
             masterPage = BasePage.initialize(WebDriverManager.getDriver(), MasterPage.class);
             mainWindow = Helper.INSTANCE.getWindow(driver);
             browseOrSearchPage = BasePage.initialize(WebDriverManager.getDriver(), BrowseOrSearchPage.class);
+            masterPage.enterTextInSearchBoxOnHomePage("Doctor");
             masterPage.clickOnSearchMagnifyingLense();
-
+            browseOrSearchPage.ClickOnArticleFromeRefineByType();
+            
             // Verifying PubMed Button
             browseOrSearchPage.clickOnFirstArticleOnSearchOrBrowsePage();
             articleCitationPage = BasePage.initialize(WebDriverManager.getDriver(), ArticleCitationPage.class);
